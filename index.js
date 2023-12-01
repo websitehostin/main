fe9a782d1c592b12e6cd24b008b727f9750afae5
websitehostin <152140953+websitehostin@users.noreply.github.com>
2023-12-01 17:20:59 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function toggleNavbar()
+{
+    const navbar = document.getElementById('navbar');
+    navbar.style.display = (navbar.style.display === 'none' || navbar.style.display === '') ? 'block' : 'none';
+}
+  
